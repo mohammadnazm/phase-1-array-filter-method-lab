f39ae7161870659129536419ce363b0cd078e8f9
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-02-29 15:10:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, query) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === query.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, query) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(query.toLowerCase())
+  );
+}
+
+function matchName(drivers, query) {
+  return drivers.filter(
+    (driver) => driver.name.toLowerCase() === query.toLowerCase()
+  );
+}
+
+const driversArray = [
+  { name: "John", hometown: "New York" },
+  { name: "Jane", hometown: "Chicago" },
+  { name: "Jim", hometown: "Los Angeles" },
+];
+
+console.log(findMatching(["John", "Jane", "Jim"], "john"));
+console.log(fuzzyMatch(["John", "Jane", "Jim"], "j"));
+console.log(matchName(driversArray, "John"));
